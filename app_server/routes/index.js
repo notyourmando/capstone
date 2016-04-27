44db3d40e7b833dbddc8e4503ab8cf5e280c2c75
Armando Robledo <armando.robledo@gmail.com>
2016-04-27 15:29:07 -0500

    Controllers and Routes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-// var homepageController = function (req, res) {
-//   res.render('index', {title: 'Express'});
-// };
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
