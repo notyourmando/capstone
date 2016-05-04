7564e27eb902a473880a54123c8922e0e894adcf
Armando Robledo <armando.robledo@gmail.com>
2016-05-04 10:00:02 -0500

    Crunch time.

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -105,31 +105,36 @@ module.exports.locationsReadOne = function(req, res) {
 /* /api/locations */
 module.exports.locationsCreate = function(req, res) {
   console.log(req.body);
+  console.log(Loc);
   Loc.create({
     name: req.body.name,
     address: req.body.address,
-    facilities: req.body.facilities.split(","),
-    coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
-    openingTimes: [{
-      days: req.body.days1,
-      opening: req.body.opening1,
-      closing: req.body.closing1,
-      closed: req.body.closed1,
-    }, {
-      days: req.body.days2,
-      opening: req.body.opening2,
-      closing: req.body.closing2,
-      closed: req.body.closed2,
-    }]
+    dates: req.body.dates,
+    day: req.body.day,
+    hours: req.body.hours,
+    link: req.body.link,
+  //   coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+  //   openingTimes: [{
+  //     days: req.body.days1,
+  //     opening: req.body.opening1,
+  //     closing: req.body.closing1,
+  //     closed: req.body.closed1,
+  //   }, {
+  //     days: req.body.days2,
+  //     opening: req.body.opening2,
+  //     closing: req.body.closing2,
+  //     closed: req.body.closed2,
+  //   }]
   }, function(err, location) {
     if (err) {
       console.log(err);
       sendJSONresponse(res, 400, err);
     } else {
       console.log(location);
-      sendJSONresponse(res, 201, location);
+      res.redirect('/location')
     }
   });
+
 };
 
 /* PUT /api/locations/:locationid */
