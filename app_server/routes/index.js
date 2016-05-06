c242e1b225b38fcbf0c3296174b0c4aa2246a706
Armando Robledo <armando.robledo@gmail.com>
2016-05-06 09:05:00 -0500

    Presentation

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,9 +5,8 @@ var ctrlOthers = require('../controllers/others');
 
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
-router.get('/location/:locationid', ctrlLocations.locationInfo);
-router.get('/location/:locationid/review/new', ctrlLocations.addReview);
-router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
